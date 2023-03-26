a0d0822600703c52032fa068562aa906351a8f02
Velimira <revekka-best@mail.ru>
2023-03-26 23:53:05 +0300

    HW

---
+++ hwtask1.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class hwtask1 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int[] array = new int[999];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (i + 1) * (i + 1) * (i + 1);
+        }
+        System.out.println("Введите два числа: ");
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        System.out.println("Кубы этих чисел равны: " + array[a - 1] + " и " + array[b - 18]);
+
+    }
+}
