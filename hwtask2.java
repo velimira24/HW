bc2cc960f1837fa7931df48d2c7e7aaca6f400be
Velimira <revekka-best@mail.ru>
2023-03-27 17:28:54 +0300

    HW

---
+++ hwtask2.java
@@ -0,0 +1,47 @@
+
+import java.util.Scanner;
+
+public class hwtask2 {
+    static int[] Random_array(int array[]) {
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (int) (Math.random() * 10);
+
+        }
+        return array;
+    }
+
+    static void Print_array(int array[]) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Введите число n:");
+        int n = scan.nextInt();
+        int[] nums = new int[n];
+        Random_array(nums);
+        Print_array(nums);
+        System.out.println(("Введите число, на которое надо умножить элементы массива"));
+        int a = scan.nextInt();
+        int nums2[] = new int[n];
+        for (int i = 0; i < nums2.length; i++) {
+            nums2[i] = nums[i] * a;
+        }
+        Print_array(nums2);
+    }
+}
+
+/*
+ * public static void main(String[] args) {
+ * try (Scanner scan = new Scanner(System.in)) {
+ * System.out.println("Введите число n:");
+ * 
+ * }
+ * 
+ * 
+ * }
+ * }
+ */
