cae6c7f57a8c79c579a2cb235c788fea0935efd0
Agniiara <119067802+velimira24@users.noreply.github.com>
2023-03-27 17:34:02 +0300

    Update hwtask2.java

--- hwtask2.java
+++ hwtask2.java
@@ -34,14 +34,3 @@ public class hwtask2 {
     }
 }
 
-/*
- * public static void main(String[] args) {
- * try (Scanner scan = new Scanner(System.in)) {
- * System.out.println("Введите число n:");
- * 
- * }
- * 
- * 
- * }
- * }
- */
